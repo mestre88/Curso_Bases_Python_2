e435383a1bd090e069bc53ed1b87ee6b9005862d
mestre88 <124459686+mestre88@users.noreply.github.com>
2023-02-17 15:12:03 +0000

    Add files via upload

---
+++ Criar_matriz.py
@@ -0,0 +1,38 @@
+def cria_matriz(num_linhas, num_colunas):
+    '''(int,int) -> matriz (lista de listas)
+    Cria e retorna uma matriz com num_linhas linhas e num_colunas
+    colunas em que cada elemento é digitado pelo utilizador
+    '''
+    matriz = [] #lista vazia
+    for i in range(num_linhas):
+        #cria a linha i
+        linha = [] # lista vazia
+        for j in range(num_colunas):
+            valor = int(input("Digite o elemento [" + str(i) + "][" + str(j) + "]"))
+            linha.append(valor)
+
+        # adiciona linha à matriz
+        matriz.append(linha)
+
+    return matriz
+
+def le_matriz():
+    lin = int(input("Digite o número de linhas da matriz: "))
+    col = int(input("Digite o número de colunas da matriz: "))
+    matriz = cria_matriz(lin, col)
+    imprime_matriz (matriz)
+    return matriz
+
+
+def imprime_matriz (matriz):
+    '''
+    Recebe matriz que é uma lista de listas e imprime a matriz da forma matematica
+    '''
+    for i in range(len(matriz)):
+        #Posição das linhas
+        j = 0
+        for j in range(len(matriz[j])):
+            #Posição das colunas
+            print(matriz[i][j], end = ' ')
+        print()
+    
